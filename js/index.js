97a5cc47bad3a5390c2def15ce289353de5c3f3b
Manishkarke <mk4345437@gmail.com>
2024-03-15 19:24:09 +0545

    Finished the section in better way

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const activateHamburger = () => {
-  const navItems = document.getElementById("nav-items");
+  const navItems = document.getElementById("header__top-nav__header__top-nav__nav-items");
 
   navItems.classList.toggle("show-menu");
 };
